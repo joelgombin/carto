5bbafdef8de06b5074fc446ff57ecdb793cd1bd3
Joel Gombin <joel.gombin@gmail.com>
2012-11-05 12:26:54 +0100

    bug réparé : oubli de retourner le résultat...

--- discretize.R
+++ discretize.R
@@ -11,4 +11,5 @@ discretize <- function(x) {
   if ((median(x) - 2*sd(x) > min(x)) & (median(x) + 2*sd(x) < max(x))) {
     d <- c(min(x),median(x) - 2*sd(x), median(x) - sd(x),median(x),median(x) + sd(x),median(x) + 2*sd(x),max(x))
   }
+  d
 }
